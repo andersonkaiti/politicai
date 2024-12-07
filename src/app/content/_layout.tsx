feb9e36fca244c7d3565f45d90518ce68e47ff5a
andersonkaiti <anderkaiti@gmail.com>
2024-12-07 19:59:00 -0300

    :bug: fix: Corrigindo a cor da Status Bar e adicionando um paddingTop na tela inicial para evitar que os ícones sobreponham os textos.

--- src/app/content/_layout.tsx
+++ src/app/content/_layout.tsx
@@ -1,76 +1,80 @@
 import { Tabs } from "expo-router";
 import "@styles/global.css";
 import { Ionicons } from "@expo/vector-icons";
+import { StatusBar } from "react-native";
 
 export default function IndexLayout() {
   return (
-    <Tabs
-      screenOptions={{
-        tabBarStyle: {
-          height: 50,
-          paddingBottom: 5,
-        },
-      }}
-    >
-      <Tabs.Screen
-        name="(attested-sites)"
-        options={{
-          title: "Sites confiáveis",
-          headerShown: false,
-          tabBarIcon: ({ color }) => (
-            <Ionicons name="globe-outline" color={color} size={24} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="help/index"
-        options={{
-          title: "Passo a passo",
-          tabBarIcon: ({ color }) => (
-            <Ionicons name="footsteps-outline" color={color} size={24} />
-          ),
-          headerStyle: {
-            backgroundColor: "#173058",
+    <>
+      <StatusBar barStyle="light-content" />
+      <Tabs
+        screenOptions={{
+          tabBarStyle: {
+            height: 50,
+            paddingBottom: 5,
           },
-          headerTintColor: "#fff",
         }}
-      />
-      <Tabs.Screen
-        name="importance/index"
-        options={{
-          title: "Importância",
-          tabBarIcon: ({ color }) => (
-            <Ionicons name="star-outline" color={color} size={24} />
-          ),
-          headerStyle: {
-            backgroundColor: "#173058",
-          },
-          headerTintColor: "#fff",
-        }}
-      />
-      <Tabs.Screen
-        name="source/index"
-        options={{
-          title: "Fontes",
-          tabBarIcon: ({ color }) => (
-            <Ionicons name="book-outline" color={color} size={24} />
-          ),
-          headerStyle: {
-            backgroundColor: "#173058",
-          },
-          headerTintColor: "#fff",
-        }}
-      />
-      <Tabs.Screen
-        name="(credits)"
-        options={{
-          title: "Créditos",
-          headerShown: false,
-          tabBarIcon: ({ color }) => (
-            <Ionicons name="information-outline" color={color} size={24} />
-          ),
-        }}
-      />
-    </Tabs>
+      >
+        <Tabs.Screen
+          name="(attested-sites)"
+          options={{
+            title: "Sites confiáveis",
+            headerShown: false,
+            tabBarIcon: ({ color }) => (
+              <Ionicons name="globe-outline" color={color} size={24} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="help/index"
+          options={{
+            title: "Passo a passo",
+            tabBarIcon: ({ color }) => (
+              <Ionicons name="footsteps-outline" color={color} size={24} />
+            ),
+            headerStyle: {
+              backgroundColor: "#173058",
+            },
+            headerTintColor: "#fff",
+          }}
+        />
+        <Tabs.Screen
+          name="importance/index"
+          options={{
+            title: "Importância",
+            tabBarIcon: ({ color }) => (
+              <Ionicons name="star-outline" color={color} size={24} />
+            ),
+            headerStyle: {
+              backgroundColor: "#173058",
+            },
+            headerTintColor: "#fff",
+          }}
+        />
+        <Tabs.Screen
+          name="source/index"
+          options={{
+            title: "Fontes",
+            tabBarIcon: ({ color }) => (
+              <Ionicons name="book-outline" color={color} size={24} />
+            ),
+            headerStyle: {
+              backgroundColor: "#173058",
+            },
+            headerTintColor: "#fff",
+          }}
+        />
+        <Tabs.Screen
+          name="(credits)"
+          options={{
+            title: "Créditos",
+            headerShown: false,
+            tabBarIcon: ({ color }) => (
+              <Ionicons name="information-outline" color={color} size={24} />
+            ),
+          }}
+        />
+      </Tabs>
+    </>
   );
 }
