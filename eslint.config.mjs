b265d3e40e626379a75a39d5ea9d4eccf1a51611
andersonkaiti <anderkaiti@gmail.com>
2026-04-17 17:15:03 -0300

    :wrench: chore(config): add eslint-prettier integration, import sort, and update prettier rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,41 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+import pluginJs from '@eslint/js'
+import eslintConfigPrettier from 'eslint-config-prettier/flat'
+import eslintPluginPrettier from 'eslint-plugin-prettier'
+import pluginReact from 'eslint-plugin-react'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
 
 export default [
   {
-    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
     languageOptions: {
       globals: globals.node,
-    },
-    rules: {
-      "react/react-in-jsx-scope": false,
     },
   },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
-];
+
+  eslintConfigPrettier,
+
+  {
+    plugins: {
+      prettier: eslintPluginPrettier,
+      'simple-import-sort': simpleImportSort,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      '@typescript-eslint/no-require-imports': 'off',
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+      'prettier/prettier': 'error',
+    },
+  },
+]
